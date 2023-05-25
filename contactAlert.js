c5bd604e77c13e74c395246c927f1d1afd975c8c
maverick4200 <joshnava2@gmail.com>
2023-05-25 00:43:25 -0500

    fixed form validation

--- contactAlert.js
+++ contactAlert.js
@@ -1,13 +1,45 @@
-document.getElementById("submit-btn").addEventListener("click", function() {
-    // Display the SweetAlert2 dialog
-    Swal.fire({
-      title: 'Thank you!',
-      text: 'Your form has been successfully submitted.',
-      icon: 'success',
-      confirmButtonText: 'OK',
-      buttonsStyling: false,
-      customClass: {
-        confirmButton: 'btn btn-success'
-      }
-    });
+document.getElementById('project-form').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  // Check if any text fields are empty
+  var textFields = document.querySelectorAll('#project-form input[type="text"]');
+  var isMissing = false;
+
+  textFields.forEach(function (textField) {
+    if (textField.value.trim() === '') {
+      isMissing = true;
+      textField.classList.add('is-invalid'); // Add Bootstrap's "is-invalid" class for visual feedback
+    } else {
+      textField.classList.remove('is-invalid');
+    }
   });
+
+  if (isMissing) {
+    // Display SweetAlert warning message if text fields are missing
+    Swal.fire({
+      icon: 'warning',
+      title: 'Missing Info',
+      text: 'Please fill out all the required text fields.',
+      confirmButtonColor: '#0B4C18',
+    });
+
+    return false; // Prevent form submission
+  } else {
+    // All text fields are filled, proceed with form submission logic here
+
+    // Display SweetAlert success message
+    Swal.fire({
+      icon: 'success',
+      title: 'Success!',
+      text: 'Form submitted successfully.',
+      confirmButtonColor: '#0B4C18',
+    });
+  }
+});
+
+
+
+
+
+
+
